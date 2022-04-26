433f842d9ec26c26122f61faeb3e0ab06a278379
JJackson510 <jamiej.jj5@gmail.com>
2022-04-26 18:26:37 -0500

    working on seeds

---
+++ seeds/feeder-seed.js
@@ -0,0 +1,24 @@
+const feederData = [
+    {
+        name: "feeder",
+        latitude: "29.749737",
+        longitude: "-95.503594"
+    },
+    {
+        name: "feeder 2",
+        latitude: "29.745227",
+        longitude: "-95.518989"
+    },
+    {
+        name: "feeder 3",
+        latitude: "29.758240",
+        longitude: "-95.512528"
+    },
+    {
+        name: "feeder 4",
+        latitude: "29.735923",
+        longitude: "-95.503531"
+    }
+];
+
+module.exports =
---
+++ seeds/hive-seeds.js
@@ -0,0 +1,24 @@
+const hiveData = [
+    {
+        name: "hive",
+        latitude: "29.749714",
+        longitude: "-95.504576"
+    },
+    {
+        name: "hive 2",
+        latitude: "29.746011",
+        longitude: "-95.519140"
+    },
+    {
+        name: "hive 3",
+        latitude: "29.756196",
+        longitude: "-95.510426"
+    },
+    {
+        name: "hive 4",
+        latitude: "29.736362",
+        longitude: "-95.508100"
+    }
+];
+
+module.exports =
